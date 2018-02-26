92c3137963d2901a8f2ec10a99f4e89f909c4bc2
Muthukumarll <35914910+Muthukumarll@users.noreply.github.com>
2018-02-26 13:39:38 +0530

    program7.c

---
+++ program7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[50],i;
+    printf("enter the string :");
+    scanf("%d",&a);
+
+   if(a[i]=='0'||'1')
+   {
+   i++;
+
+printf("yes\n");
+   }
+
+    else
+    {
+
+    i++;
+printf("no");
+    }
+getch();
+}
